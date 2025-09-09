f0c2b1036b7fdfdc9ae91bd178bef255c634ca78
Matheus0351996 <mnatalsantos@hotmail.com>
2025-09-08 21:56:34 -0300

    alinhamento

--- src/app/_components/testimonials.tsx
+++ src/app/_components/testimonials.tsx
@@ -5,7 +5,7 @@ export function Testimonials(){
     return (
         <section className="bg-amber-300">
             <div>{/* Container Pai */}
-                <div>
+                <div className="flex justify-center">
                     <h2 className="text-2xl font-bold">Depoimento dos nossos clientes</h2> 
                 </div>
             <div className="bg-[#1e293b] text-white">{/*  */}
